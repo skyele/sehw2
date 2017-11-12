e899697e2a7a97169f2f2c048a63ddfd734064b9
skyele <skyele@sjtu.edu.cn>
2017-11-12 22:52:47 +0800

    basic recipes + delete funtion

--- fcc-recipe/src/App.js
+++ fcc-recipe/src/App.js
@@ -1,18 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Accordion from 'react-bootstrap/lib/Accordion'
+import Panel from 'react-bootstrap/lib/Panel'
+import Button from 'react-bootstrap/lib/Button'
+import ButtonToolbar from 'react-bootstrap/lib/ButtonToolbar'
+import Modal from 'react-bootstrap/lib/Modal'
+import FormGroup from 'react-bootstrap/lib/FormGroup'
+import ControlLabel from 'react-bootstrap/lib/ControlLabel'
+import FormControl from 'react-bootstrap/lib/FormControl'
 
 class App extends Component {
+state ={
+  recipes:[
+      {recipeName:'Sky1',ingredients:['SJTUer1','Charming','MEGA SMART']},
+      {recipeName:'Sky2',ingredients:['SJTUer2','Charming','MEGA SMART']},
+      {recipeName:'Sky3',ingredients:['SJTUer3','Charming','MEGA SMART']}
+  ]
+}
+
+//Delete a recipe
+deleteRecipe(index){
+let recipes=this.state.recipes.slice();
+recipes.splice(index,1);
+this.setState({recipes});
+}
+
   render() {
+    const{recipes}= this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App container">
+        <Accordion>
+          {recipes.map((recipe,index) => (
+<Panel header={recipe.recipeName} eventkey={index} key= {index}>
+<o1>
+       {recipe.ingredients.map((item)=>(
+           <li key={item}> {item} </li>
+       ))}
+</o1>
+<ButtonToolbar>
+<Button bsStyle="danger" onClick={(event)=>this.deleteRecipe(index)}>Delete Recipe</Button>
+      <Button bsStyle="default">Edit Recipe</Button>
+      </ButtonToolbar>
+</Panel>
+
+
+          ))}
+        </Accordion>
+
+<Modal show={this.state.showAdd}></Modal>
+
+
+          <Button bsStyle="primary">Add Recipe</Button>
       </div>
     );
   }
